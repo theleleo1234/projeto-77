3fe99cbbc1f8431669051f58e694184c796a9431
theleleo1234 <123700737+theleleo1234@users.noreply.github.com>
2023-04-28 12:17:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("meu primeiro projeto no console");
+     var firstname = "ranbir";
+     var lastname = "kapoor";
+
+     var fullname = firstname.concat(lastname);
+     console.log(fullname);
+
+     var a = 10;
+     var b = 5;
+
+     var add = a + b ;
+     console.log(add);
+
+     var sub = a - b ;
+     console.log(sub);
+
+     var multiple = a * b ;
+     console.log(multiple);
+
+
+     var divide = a / b ;
+     console.log(divide);
